6500d0dc9bddcc2e489745add602b7e91c2e4a5b
Eunseong, Yeo <e7838752@gmail.com>
2022-02-17 17:35:04 +0900

    ✨ feat: create add todo
    1. get value from todo input
    2. add todo to todos
    3. reset input

--- todo-v0/src/js/app.js
+++ todo-v0/src/js/app.js
@@ -1,7 +1,8 @@
 const $toggleAll = document.querySelector('.toggle-all');
+const $todoInput = document.querySelector('.todo-input');
 const $todoList = document.querySelector('.todo-list');
 
-// todo => { id: number, content: string, checked: boolean}
+// todo: { id: number, content: string, checked: boolean}
 let todos = [];
 
 const render = () => {
@@ -15,6 +16,25 @@ const render = () => {
   );
 };
 
-window.onDomReady = () => {
+const setTodo = _todos => {
+  todos = [..._todos];
   render();
 };
+
+const getNextId = () => Math.max(...todos.map(todo => todo.id), 0) + 1;
+
+const addTodo = todo => {
+  const _todos = [{ id: getNextId(), content: todo, checked: false }, ...todos];
+  setTodo(_todos);
+};
+
+$todoInput.onkeyup = e => {
+  const { code } = e;
+  const todo = e.target.value.trim();
+  if (code !== 'Enter' || todo === '') return;
+
+  e.target.value = '';
+  addTodo(todo);
+};
+
+document.addEventListener('DOMContentLoaded', render);
