64b3c99b141b0c5b99ff9cb0f7da9d31e7500b1e
Eunseong, Yeo <e7838752@gmail.com>
2022-02-17 18:02:32 +0900

    ✨ feat: create remove todo
    1. filter by id
    2. set todo

--- todo-v0/src/js/app.js
+++ todo-v0/src/js/app.js
@@ -1,6 +1,7 @@
 const $toggleAll = document.querySelector('.toggle-all');
 const $todoInput = document.querySelector('.todo-input');
 const $todoList = document.querySelector('.todo-list');
+const $count = document.querySelector('.count');
 
 // todo: { id: number, content: string, checked: boolean}
 let todos = [
@@ -16,10 +17,13 @@ const render = () => {
     <li data-id="${id}" class="todo-item">
       <input class="toggle" type="checkbox" ${checked ? 'checked' : ''} />
       <span>${content}</span>
+      <button class="remove" type="button">x</button>
     </li>
   `
     )
     .join('');
+
+  $count.textContent = `${todos.length} item${todos.length > 1 ? 's' : ''} left`;
 };
 
 const setTodo = _todos => {
@@ -45,14 +49,19 @@ const toggleAll = checked => {
   setTodo(_todos);
 };
 
+const removeTodo = id => {
+  const _todos = todos.filter(todo => todo.id !== +id);
+  setTodo(_todos);
+};
+
 $toggleAll.onclick = ({ target: { checked } }) => {
   toggleAll(checked);
 };
 
 $todoList.onclick = ({ target }) => {
-  if (!target.matches('.toggle')) return;
+  if (!(target.matches('.toggle') || target.matches('.remove'))) return;
   const { id } = target.closest('.todo-item').dataset;
-  toggle(id);
+  target.matches('.toggle') ? toggle(id) : removeTodo(id);
 };
 
 $todoInput.onkeyup = e => {
