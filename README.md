5e1238217ce6fba5a042cd82be619b0153712eae
Eunseong, Yeo <e7838752@gmail.com>
2022-02-18 18:20:32 +0900

    ✨ feat: create data file
    read data from todos.json
    update todos.json when change detected

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 - CRUD with todo app
 - Basic template for next version
 - Using static todo data in JS file
+
+2. v1.0
+
+- CRUD with todo app
+- using express for server
+- get todos from todos.json
+- edit todos.json with changing todos
--- todo-v1.0/backend/server.js
+++ todo-v1.0/backend/server.js
@@ -1,14 +1,15 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 const port = 9000;
 
-// Mock data
-let todos = [
-  { id: 3, content: 'Javascript', completed: false },
-  { id: 2, content: 'CSS', completed: true },
-  { id: 1, content: 'HTML', completed: false },
-];
+let todos = JSON.parse(fs.readFileSync(`${__dirname}/data/todos.json`));
+
+const setTodos = _todos => {
+  todos = [..._todos];
+  fs.writeFileSync(`${__dirname}/data/todos.json`, JSON.stringify(todos));
+};
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -19,14 +20,13 @@ app.get('/todos', (req, res) => {
 
 app.post('/todos', (req, res) => {
   console.log(req.body);
-  todos = [req.body, ...todos];
+  setTodos([req.body, ...todos]);
   res.send(todos);
 });
 
 app.patch('/todos', (req, res) => {
   const { completed } = req.body;
-
-  todos = todos.map(todo => ({ ...todo, completed }));
+  setTodos(todos.map(todo => ({ ...todo, completed })));
   res.send(todos);
 });
 
@@ -34,21 +34,23 @@ app.patch('/todos/:id', (req, res) => {
   const { id } = req.params;
   const payload = req.body;
 
-  todos = todos.map(todo => (todo.id === +id ? { ...todo, ...payload } : todo));
+  setTodos(
+    todos.map(todo => (todo.id === +id ? { ...todo, ...payload } : todo))
+  );
   res.send(todos);
 });
 
 app.delete('/todos/:id', (req, res) => {
   const { id } = req.params;
 
-  todos = todos.filter(todo => todo.id !== +id);
+  setTodos(todos.filter(todo => todo.id !== +id));
   res.send(todos);
 });
 
 app.delete('/todos', (req, res) => {
   const { completed } = req.query;
 
-  todos = todos.filter(todo => todo.completed !== JSON.parse(completed));
+  setTodos(todos.filter(todo => todo.completed !== JSON.parse(completed)));
   res.send(todos);
 });
 
